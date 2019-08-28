bf1b4fa97cfe3a29a150cf35ab8165b7b6e2dbd3
Anselmo Rossiello <Anselmo.Rossiello@quartech.com>
2019-08-28 15:40:47 -0700

    Cleaned up unneeded code in WorksheetView

--- app/backend/arm/views/WorksheetView.py
+++ app/backend/arm/views/WorksheetView.py
@@ -39,82 +39,21 @@ class WorksheetForm( Form ):
         cleaned_data = super( WorksheetForm, self ).clean()
         return cleaned_data
 
-
-def format_submission( sdict ):
-
-    worksheetData = WorksheetData(
-        sdict.get( 'dairy_farm_name', '' ) ,
-        sdict.get( 'apply_date', '' ) , 
-        sdict.get( 'managment_unit', '' ) ,
-        sdict.get( 'precipitation_1', '' ) ,
-        sdict.get( 'precipitation_2', '' ) ,
-        sdict.get( 'water_table_depth', '' ) ,
-        sdict.get( 'forage_height', '' ) ,
-        sdict.get( 'soil_type', '' ) ,
-        sdict.get( 'soil_moisture', '' ) ,
-        sdict.get( 'forage_density', '' ) ,
-        sdict.get( 'application_equipment', '' ) ,
-        sdict.get( 'critical_area', '' ) ,
-        sdict.get( 'manure_setback_distance', '' ) ,
-        sdict.get( 'surface_condition', '' ) ,)
-
-    return worksheetData
-
-
-def generate_pdf(worksheetData):
-
-    # Rendered
-    html_string = render_to_string('pdf.html', {'WorksheetData': worksheetData})
-    weasy_server = ("%s/pdf?filename=arm_report.pdf" % config('WEASYPRINT_URL'))
-
-    response = requests.post(weasy_server, data=html_string) 
-
-    return response
-
-
 class WorksheetView( FormView ):
 
     template_name = 'worksheet.html'
     form_class = WorksheetForm 
 
-    http_method_names = [ 'get', 'post', ]
+    http_method_names = [ 'get', ]
 
     def get( self, request, *args, **kwargs ):
 
         data = {
-                'page_name': 'ARM Worksheet',
+                'page_name': 'ARM Worksheet'
         }
         data.update( csrf( request ) )
 
         return render_to_response( self.template_name, data )
 
-    def set_success_url( self ):
-        self.success_url = reverse( 'thankyou', )
-
-    def form_valid( self, form ):
-        worksheetData = format_submission( self.request.POST )
-        
-        #return super( WorksheetView, self ).form_valid( form )
-        # response = generate_pdf(worksheetData)
-        # print(response.headers)
-        
-        html_string = render_to_string('pdf.html', {'WorksheetData': worksheetData})
-
-        response = HttpResponse(content_type='text/html;')
-        response.write(html_string)
-        return response
-
-    def post( self, request, *args, **kwargs ):
-
-        logger.debug( "[ POST PARAMS ]: %s" % json.dumps( request.POST, indent=4 ) )
-
-        form_class = self.get_form_class()
-        form = self.get_form(form_class)
-
-        if form.is_valid():
-            self.set_success_url()
-            return self.form_valid(form)
-        else:
-            return self.form_invalid(form)
 
 
